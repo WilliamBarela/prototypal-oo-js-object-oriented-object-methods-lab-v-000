fd5169b26c64b25077fd70b61036c8ea401b894b
William Barela <william.barela.dev@gmail.com>
2020-05-26 14:26:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ BoardMember.prototype.releasePressStatement = () => {
 };
 
 BoardMember.prototype.sayHi = () => {
-  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-}
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
+};
