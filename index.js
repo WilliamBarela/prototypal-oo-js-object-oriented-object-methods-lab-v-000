9990f2c38973491228546741aea00bb946148ee7
William Barela <william.barela.dev@gmail.com>
2020-05-26 14:19:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ function BoardMember(name, homeState, training) {
   this.training = training;
 }
 
-BoardMember.prototype.veto = function (){ return “No, I must disagree”; }
+BoardMember.prototype.veto = function (){ 
+  return 'No, I must disagree';
+};
