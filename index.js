7b273094ce68f40202d7419228039e800aeb59e0
William Barela <william.barela.dev@gmail.com>
2020-05-26 14:25:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,14 @@ BoardMember.prototype.approve = () => {
   return 'You can do that!';
 };
 
+BoardMember.prototype.doCharity = () => {
+  return 'I like to help people.';
+};
+
+BoardMember.prototype.releasePressStatement = () => {
+  return 'You will see great things from Scuber';
+};
+
+BoardMember.prototype.sayHi = () => {
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+}
